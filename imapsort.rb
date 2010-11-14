ba6a8cc03b6b1762c8aa10418450df4042b15533
Sergej Braznikov <sbraznikov@me.com>
2010-11-14 13:40:00 +0100

    Code cleanup.

--- imapsort.rb
+++ imapsort.rb
@@ -1,38 +1,38 @@
 require 'net/imap'
 
 class Imapsort
-  def initialize &block
-    instance_eval &block
-  end
-
-  def login account
-    imap = Net::IMAP.new(account[:server])
-    imap.authenticate('LOGIN', account[:user], account[:pass])
-    imap.select('INBOX')
-    @imap = imap
-  end
-  
-  def from email
-    @result = @imap.search(['FROM', email])
-  end
-  
-  def move path
-    @result.each do |message_id|
-      @imap.copy(message_id, path)
-      @imap.store(message_id, '+FLAGS', [:Deleted])
+    def initialize &block
+        instance_eval &block
     end
-    @imap.expunge
-  end
 
-  def remove
-    mark('Deleted')
-    @imap.expunge
-  end
-
-  def mark flag
-    flags = [:"#{flag}"]
-    @result.each do |message_id|
-      @imap.store(message_id, '+FLAGS', flags)
+    def login account
+        imap = Net::IMAP.new(account[:server])
+        imap.authenticate('LOGIN', account[:user], account[:pass])
+        imap.select('INBOX')
+        @imap = imap
     end
-  end
+  
+    def from email
+        @result = @imap.search(['FROM', email])
+    end
+  
+    def move path
+        @result.each do |message_id|
+            @imap.copy(message_id, path)
+            @imap.store(message_id, '+FLAGS', [:Deleted])
+        end
+        @imap.expunge
+    end
+
+    def remove
+        mark('Deleted')
+        @imap.expunge
+    end
+
+    def mark flag
+        flags = [:"#{flag}"]
+        @result.each do |message_id|
+            @imap.store(message_id, '+FLAGS', flags)
+        end
+    end
 end
