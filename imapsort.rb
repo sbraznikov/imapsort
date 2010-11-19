dc720761b31e7f3c6384e06e97df62c2c268c86f
Sergej Braznikov <sbraznikov@me.com>
2010-11-19 18:00:16 +0100

    Added logging support.

--- imapsort.rb
+++ imapsort.rb
@@ -1,10 +1,12 @@
 require 'net/imap'
+require 'logger'
 
 class Imapsort
     def initialize &block
+        @log = Logger.new(STDOUT)
         instance_eval &block
         @imap.logout
-        @imap.disconnect
+        # @imap.disconnect
     end
 
     def login account
@@ -16,28 +18,34 @@ class Imapsort
   
     def from email
         @result = @imap.search(['FROM', email])
+        # @log.info("from #{email}")
     end
 
     def to email
         @result = @imap.search(['TO', email])
+        @log.info("to #{email}")
     end
 
     def subject string
         @result = @imap.search(['SUBJECT', string])
+        @log.info("subject #{string}")
     end
     
     def body string
         @result = @imap.search(['BODY', string])
+        @log.info("body #{string}")
     end
 
-    def cc string
-        @result = @imap.search(['CC', string])
+    def cc email
+        @result = @imap.search(['CC', email])
+        @log.info("from #{email}")
     end
     
     def move path
         @result.each do |message_id|
             @imap.copy(message_id, path)
             @imap.store(message_id, '+FLAGS', [:Deleted])
+            @log.info("move #{message_id}")
         end
         @imap.expunge
     end
@@ -51,6 +59,7 @@ class Imapsort
         flags = [:"#{flag}"]
         @result.each do |message_id|
             @imap.store(message_id, '+FLAGS', flags)
+            @log.info("mark #{message_id}")
         end
     end
 end
