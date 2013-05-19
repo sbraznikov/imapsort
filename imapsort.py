f770f3fd306381b8db64adccb69909101e5e192e
Sergej Braznikov <sbraznikov@me.com>
2013-05-19 20:15:39 +0200

    Aded YAML support for configuration and rules.

--- imapsort.py
+++ imapsort.py
@@ -1,37 +1,50 @@
 import imaplib
+import yaml
+
+
+CONFIG_PATH = 'imapsort.rc'
 
 
 class Imapsort(object):
-    def __init__(self, server, user, password, rules):
-        self.init_imap(server, user, password)
-        self.init_rules(rules)
+    def __init__(self):
+        self.init_config()
+        self.init_imap()
+        self.init_rules()
 
-    def init_imap(self, server, user, password):
-        imap = imaplib.IMAP4(server)
-        imap.login(user, password)
+    def init_config(self):
+        config_path = open(CONFIG_PATH)
+        self.config = yaml.load(config_path)
+        config_path.close()
+
+    def init_imap(self):
+        config = self.config.pop('imap', None)
+        if config['ssl'] is True:
+            imap = imaplib.IMAP4_SSL(config['server'], config['port'])
+        else:
+            imap = imaplib.IMAP4(config['server'])
+        imap.login(config['user'], config['password'])
         imap.select()
         self.imap = imap
 
-    def init_rules(self, rules):
-        self.rules = rules
-        self.walk()
+    def init_rules(self):
+        rules = self.config
+        [self.run(rule) for rule in rules.iteritems()]
 
-    def walk(self):
-        [self.run(rule[0], rule[1]) for rule in self.rules]
-
-    def run(self, command, param):
-        method_name = 'email_%s' % command
-        if hasattr(self, method_name):
-            method = getattr(self, method_name)
-            method(param)
-        else:
-            self.error(command)
+    def run(self, rule):
+        commands = []
+        commands.insert(0, rule[1]['conditions'])
+        commands.insert(1, rule[1]['actions'])
+        for command in commands:
+            for command, param in command.iteritems():
+                method_name = 'email_%s' % command
+                if hasattr(self, method_name):
+                    method = getattr(self, method_name)
+                    method(param)
+                else:
+                    self.error(command)
 
     def error(self, command):
         print 'Imapsort: command not found: %s' % command
-
-    def prepare_result(self, response):
-        self.result = ','.join(response.split(' '))
 
     def search(self, key, pattern, is_header=False):
         if not is_header:
@@ -41,7 +54,7 @@ class Imapsort(object):
         typ, [response] = self.imap.search(None, search)
         if typ != 'OK':
             raise RuntimeError(response)
-        self.prepare_result(response)
+        self.result = ','.join(response.split(' '))
 
     def email_from(self, email):
         self.search('FROM', email)
@@ -61,13 +74,16 @@ class Imapsort(object):
     def email_header(self, string):
         self.search('HEADER', string, is_header=True)
 
-    def email_action(self, action):
-        self.run(action[0], action[1])
-
     def email_mark(self, flag):
+        if len(self.result) <= 0:
+            return
         self.imap.store(self.result, '+FLAGS', r'(\%s)' % flag)
 
     def email_move(self, path):
+        if len(self.result) <= 0:
+            return
         self.imap.copy(self.result, path)
         self.imap.store(self.result, '+FLAGS', r'(\Deleted)')
         self.imap.expunge()
+
+Imapsort()
